10aa33adfb8b506217ca405d7c587a8622fb5fcb
Valentin Hilbig <webmaster@scylla-charybdis.com>
2019-08-25 21:15:18 +0200

    setup.sh modified

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -x
+
 URL="$(git config --get remote.origin.url)"
 
 git init config
 cd config
 git submodule add "$URL" gen
 
-ln -s gen/{Makefile,example.org,letsencrypt.http} .
+ln -vs gen/{Makefile,example.org,letsencrypt.http} .
 
